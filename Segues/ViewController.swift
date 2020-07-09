7d1c9c077f80d2ee91ead8572f6352fff882ca84
Bethany Chance <bethanywchance@gmail.com>
2020-07-09 12:09:19 -0400

    Initial Commit

---
+++ Segues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Segues
+//
+//  Created by Bethany Chance on 7/9/20.
+//  Copyright © 2020 KWK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
